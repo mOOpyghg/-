5c063c696ea9f13678cdfc59d8f6f8e3a0c047ee
mOOpyghg <moopyghg@gmail.com>
2024-12-06 01:36:16 +0300

    Game

---
+++ WindowsFormsApp14/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace WindowsFormsApp14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblScore.Location = new System.Drawing.Point(12, 22);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(71, 20);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Счет: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Игра";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp14/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        private readonly string saveFilePath = "saved_data.txt";
+        private int score = 0;
+        private int itemX;
+        private int itemY;
+        private int plateX;
+        private const int plateWidth = 75; 
+        private const int plateHeight = 20;
+        private Random random = new Random();
+        private Image cupcakeImage;
+        private Image heartImage;
+        private Image plateImage;
+        private int lives = 3; 
+        private const string FileName = "data.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            LoadCupcakeImage();
+            LoadHeartImage(); 
+            LoadPlateImage(); 
+            StartNewGame();
+        }
+
+        private void LoadCupcakeImage()
+        {
+            try
+            {
+                cupcakeImage = Image.FromFile(@"C:\Mac\Home\Desktop\флеш 2.0\Игра\WindowsFormsApp14\icons8-череп-64.png");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка загрузки изображения: " + ex.Message);
+                this.Close();
+            }
+        }
+
+        private void LoadHeartImage()
+        {
+            try
+            {
+                heartImage = Image.FromFile(@"C:\Mac\Home\Desktop\флеш 2.0\Игра\WindowsFormsApp14\icons8-сердце-60.png"); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка загрузки изображения сердца: " + ex.Message);
+                this.Close(); 
+            }
+        }
+
+        private void LoadPlateImage() 
+        {
+            try
+            {
+                plateImage = Image.FromFile(@"C:\Mac\Home\Desktop\флеш 2.0\Игра\WindowsFormsApp14\icons8-мусорный-контейнер-64.png");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка загрузки изображения: " + ex.Message);
+                this.Close(); 
+            }
+        }
+
+        private void StartNewGame()
+        {
+            score = 0;
+            plateX = (this.ClientSize.Width - plateWidth) / 2; 
+            itemX = random.Next(0, this.ClientSize.Width - cupcakeImage.Width);
+            itemY = 0;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(saveFilePath))
+            {
+                
+                string savedNumber = File.ReadAllText(saveFilePath);
+                label1.Text = savedNumber;
+            }
+            else
+            {
+                label1.Text = "Файл отсутствует";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            itemY += 15;
+            if (itemY > this.ClientSize.Height)
+            {
+                itemY = 0;
+                itemX = random.Next(0, this.ClientSize.Width - cupcakeImage.Width);
+
+               
+                lives--;
+                if (lives <= 0)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Игра окончена! Ваш счет: " + score);
+                    this.Close(); 
+                }
+            }
+
+            
+            if (itemY + cupcakeImage.Height >= this.ClientSize.Height - plateHeight - 30 && 
+                                itemX + cupcakeImage.Width > plateX && itemX < plateX + plateWidth)
+            {
+                score++;
+                itemY = 0;
+                itemX = random.Next(0, this.ClientSize.Width - cupcakeImage.Width);
+            }
+
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawImage(plateImage, plateX, this.ClientSize.Height - plateHeight - 90);
+            e.Graphics.DrawImage(cupcakeImage, itemX, itemY); 
+
+            
+            for (int i = 0; i < lives; i++)
+            {
+                e.Graphics.DrawImage(heartImage, this.ClientSize.Width - (i + 1) * heartImage.Width - 10, 10); 
+            }
+
+            lblScore.Text = "Счет: " + score;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (e.KeyCode == Keys.Left && plateX > 0)
+            {
+                plateX -= 20;
+            }
+            else if (e.KeyCode == Keys.Right && plateX < this.ClientSize.Width - plateWidth) 
+            {
+                plateX += 20; 
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(saveFilePath, lblScore.Text);
+        }
+    }
+}
+
